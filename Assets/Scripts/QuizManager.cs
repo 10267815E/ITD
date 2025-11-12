18da167af044aaba59157f1b2fe2ac44f1d2a1d1
10267815E <s10267815@connect.np.edu.sg>
2025-11-12 16:57:44 +0800

    Created quiz manager object and it's respective script

---
+++ Assets/Scripts/QuizManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using OpenCover.Framework.Model;
+
+public class QuizManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public class Question
+    {
+        public string questionText;
+        public string[] options;
+        public int correctOptionIndex;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
